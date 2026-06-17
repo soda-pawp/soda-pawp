8b6763fb19c5cccda50071b1b64467cfc9705a9b
soda-pawp <bobyiscool2028@gmail.com>
2026-06-16 21:06:43 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// ★★ Commissions status ★★
+// Kailey: change this to true when you're OPEN, false when CLOSED. That's the only thing to edit!
+window.COMMISSIONS_OPEN = false;
---
+++ images.js
@@ -0,0 +1,19 @@
+// ★ Soda Pawp image list ★
+// Paste each Google Drive FILE ID here (right-click file → Share → copy link → the bit between /d/ and /view).
+// These photos auto-fill the Home, Gallery, and Shop grids.
+window.SODA_IMAGES = [
+  '1_Yi3NW83zgLQcGPYFHrO5kv4ut8SPDHz',
+  '1VeCYGpZDs0bQPS1GvP_DWx7tiQoSWkLu',
+  '1pGddV_qBCjqDl3qPdSse4vfP38cu7BHS',
+  '1P8AXI6a-_jxTgZbq_fQvjYzX1S8CGq7Y',
+  '1tnQ5qiHyZyoy2g3p7zmuGhawKl7HOCjb',
+  '1Nf5V_Lw273c8Q76nydtBHLFZIeqgKdS1',
+  '1lGgT4pDJdQ1IXsdc43hLW14_NZ80tzLH',
+  '1lCoq3Z3RXPoAVEL8Ii0LK1mZQUITSmUo',
+  '1TccC4xf-IW0G25NeOW7QXvC7P9Hj5kC7',
+  '1wbUaSZK2WJMv8r3fHQ217optpF_TnlqF',
+  '14H7INA88B1ZfJt29zcEukSjVDOR6Ynow',
+  '12iWD4xhK43XJKOXsHAkf8pguJyikH9F5',
+  '1jqQY62Oi_NJ4t28jOxPSVvRiuWqMLSx1'
+];
+window.driveUrl = id => `https://drive.google.com/thumbnail?id=${id}&sz=w1000`;
